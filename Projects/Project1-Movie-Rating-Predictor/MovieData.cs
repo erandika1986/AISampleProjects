833b8dfb80408995b1d9543c31c65d37d60d89a8
Erandika Sandaruwan <erandika1986@gmail.com>
2025-12-14 12:46:41 +0530

    feat : Project1-Movie-Rating-Predictor implementation completed.

--- Projects/Project1-Movie-Rating-Predictor/MovieData.cs
+++ Projects/Project1-Movie-Rating-Predictor/MovieData.cs
@@ -1,10 +1,19 @@
-﻿namespace Project1_Movie_Rating_Predictor
+﻿using Microsoft.ML.Data;
+
+namespace Project1_Movie_Rating_Predictor
 {
     public class MovieData
     {
+        [LoadColumn(0)]
         public float Budget { get; set; }
+
+        [LoadColumn(1)]
         public float ActorPopularity { get; set; }
+
+        [LoadColumn(2)]
         public string Genre { get; set; }
+
+        [LoadColumn(3)]
         public float Rating { get; set; }
     }
 }
--- Projects/Project1-Movie-Rating-Predictor/Program.cs
+++ Projects/Project1-Movie-Rating-Predictor/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.ML;
+using Project1_Movie_Rating_Predictor;
+
+var mlContext = new MLContext();
+
+var data = mlContext.Data.LoadFromTextFile<MovieData>(
+    "movies_10000.csv",
+    hasHeader: true,
+    separatorChar: ','
+);
+
+// Prepare pipeline
+var pipeline =
+    mlContext.Transforms.Categorical.OneHotEncoding(
+        outputColumnName: "GenreEncoded",
+        inputColumnName: "Genre")
+    .Append(mlContext.Transforms.Concatenate("Features", "Budget", "ActorPopularity", "GenreEncoded"))
+    .Append(mlContext.Regression.Trainers.FastTree(
+        labelColumnName: "Rating",
+        featureColumnName: "Features"));
+
+// Train model
+var model = pipeline.Fit(data);
+
+// Save model
+mlContext.Model.Save(model, data.Schema, "MovieRatingModel.zip");
+
+Console.WriteLine("Model trained and saved!");
+
+var predictor = mlContext.Model.CreatePredictionEngine<MovieData, MoviePrediction>(model);
+
+var input = new MovieData
+{
+    Budget = 120000000,
+    ActorPopularity = 0.9f,
+    Genre = "Action"
+};
+
+var result = predictor.Predict(input);
+
+Console.WriteLine($"Predicted Rating: {result.Score}");
+
+Console.ReadLine();
