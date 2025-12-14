e8d66d982f303351e0bba82c9bdcd37e0af3c21a
Erandika Sandaruwan <erandika1986@gmail.com>
2025-12-14 11:15:47 +0530

    Add project files.

---
+++ Projects/Project1-Movie-Rating-Predictor/MovieData.cs
@@ -0,0 +1,10 @@
+﻿namespace Project1_Movie_Rating_Predictor
+{
+    public class MovieData
+    {
+        public float Budget { get; set; }
+        public float ActorPopularity { get; set; }
+        public string Genre { get; set; }
+        public float Rating { get; set; }
+    }
+}
---
+++ Projects/Project1-Movie-Rating-Predictor/MoviePrediction.cs
@@ -0,0 +1,7 @@
+﻿namespace Project1_Movie_Rating_Predictor
+{
+    public class MoviePrediction
+    {
+        public float Score { get; set; }
+    }
+}
